18e2162249fcf06d41a94e6436bc6b6fc577b07d
wangchangjing <wangcj@z-code.cn>
2018-06-05 11:35:08 +0800

    2018年6月5日 11:34:46 commmit

---
+++ hello-spring-cloud-eureka/src/main/java/com/funtl/hello/spring/cloud/eureka/EurekaApplication.java
@@ -0,0 +1,19 @@
+package com.funtl.hello.spring.cloud.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author wangchangjing
+ * @Description:
+ * @ClassName: EurekaApplication
+ * @date 2018/6/5 10:23
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+}
