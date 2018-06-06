108d28f05e443fa8c07ba44098a3ff88fcfeaf14
wangchangjing <1071499071@qq.com>
2018-06-06 23:45:37 +0800

    2018年6月6日23:44:29

---
+++ hello-spring-cloud-service-admin/src/main/java/com/funtl/hello/spring/cloud/service/admin/controller/ServiceAdminController.java
@@ -0,0 +1,14 @@
+package com.funtl.hello.spring.cloud.service.admin.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ServiceAdminController {
+    @RequestMapping(value = "hi", method = RequestMethod.GET)
+    public String sayHi(@RequestParam String message) {
+        return "Hi，your message is :"+message+" i am from port：8763";
+    }
+}
